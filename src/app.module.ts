0793c26ab38ee4cc89b82242b53f145a6168dc8e
Konstantin Maksimovich <kmaksimovich@yandex.ru>
2021-03-21 17:50:39 +0300

    'initial'

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+
+@Module({
+  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
